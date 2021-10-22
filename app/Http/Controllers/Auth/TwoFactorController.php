06933bb33de846f16d2f24a5c2268e487f8ac52f
Raheel Hsn <Rah1x@users.noreply.github.com>
2021-10-22 20:22:57 +1100

    Update TwoFactorController.php
    Added the missing link for TwoFactorCode()

--- app/Http/Controllers/Auth/TwoFactorController.php
+++ app/Http/Controllers/Auth/TwoFactorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Notifications\TwoFactorCode;
 use Illuminate\Http\Request;
+use App\Notifications\TwoFactorCode;
 
 class TwoFactorController extends Controller
 {
